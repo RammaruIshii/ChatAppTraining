6c104bfdee454713176ccf8c821308211aad1bd8
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-06 21:17:07 +0900

    ChatList周りの準備完了

---
+++ ChatAppTraining/ChatListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListViewController.swift
+//  ChatAppTraining
+//
+//  Created by USER on 2022/09/06.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
