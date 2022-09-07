5217b83a44bbc32ceedef1e59665d324d9013c8d
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-07 11:32:18 +0900

    一旦cell関連準備完了

--- ChatAppTraining/ChatListViewController.swift
+++ ChatAppTraining/ChatListViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
-
+    @IBOutlet weak var chatListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+}
 
+class ChatListTableViewCell: UITableViewCell {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var massageLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    //cell呼ぶのに必要なもの
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        userImageView.layer.cornerRadius = 37
+    }
+
+    //tableView呼ぶのに必要なもの
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
 }
