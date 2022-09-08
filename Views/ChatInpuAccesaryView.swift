1f1cf846464d64dc297deaebb41d923bec7cbe17
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-08 18:45:45 +0900

    ChatInputAccesaryVの上にChatInputAccesaryV.xibを背セットする記述

---
+++ Views/ChatInpuAccesaryView.swift
@@ -0,0 +1,36 @@
+//
+//  ChatInpuAccesaryView.swift
+//  ChatAppTraining
+//
+//  Created by USER on 2022/09/08.
+//
+
+import UIKit
+
+class ChatInputAccesaryView: UIView {
+    
+//    カスタムUIViewやUIButtonを作成した時に、initメソッド内で定数や配列の初期化などを行いたい
+//初期化というのは何か値を与えること
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibInit()
+        
+    }
+    
+    //カスタムビューの作り方。カスタムビューを作ることでUIを使い回すことができる。（xibのファイルをこのViewの上にセットしたいため記述）
+    private func nibInit() {
+        let nib = UINib(nibName: "ChatInpuAccesaryView", bundle: nil)
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
+        
+        view.frame = self.bounds
+        //UIViewがサイズ変更、向きの変更、テーブルビューセルでの編集コントロールの表示などによってスーパービューが変更されたときに、適切にシフトおよびサイズ変更することです。
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.addSubview(view)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
