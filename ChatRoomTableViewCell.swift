b7673840797a6e08ecc5277a271e3a00bf1eb7fe
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-08 14:07:25 +0900

    ChatRoomTableViewcell関連追加

---
+++ ChatRoomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRoomTableViewCell.swift
+//  ChatAppTraining
+//
+//  Created by USER on 2022/09/08.
+//
+
+import UIKit
+
+class ChatRoomTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
