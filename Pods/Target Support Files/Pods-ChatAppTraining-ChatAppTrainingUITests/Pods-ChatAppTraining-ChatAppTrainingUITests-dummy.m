6948a030ef224a0e7fd5175e9d99b6165ff9684e
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-21 12:52:00 +0900

    Firebase紐付け

---
+++ Pods/Target Support Files/Pods-ChatAppTraining-ChatAppTrainingUITests/Pods-ChatAppTraining-ChatAppTrainingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatAppTraining_ChatAppTrainingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChatAppTraining_ChatAppTrainingUITests
+@end
---
+++ Pods/Target Support Files/Pods-ChatAppTraining/Pods-ChatAppTraining-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatAppTraining : NSObject
+@end
+@implementation PodsDummy_Pods_ChatAppTraining
+@end
