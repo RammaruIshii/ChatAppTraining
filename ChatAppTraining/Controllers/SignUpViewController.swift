fdf96ec17cded4dd3c2244209775818ca096bd22
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-21 17:43:49 +0900

    ↓とSignVC紐付け

---
+++ ChatAppTraining/Controllers/SignUpViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpViewController.swift
+//  ChatAppTraining
+//
+//  Created by USER on 2022/09/21.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var profileImageButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var alreadyHaveAccountButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
