42bf5454c9077c89527d2b2eb163a848f04222e7
石井蘭丸 <ranmaru.ishii@talkoot.co.jp>
2022-09-28 19:41:27 +0900

    UserListTableView紐付け

---
+++ ChatAppTraining/Controllers/UserListViewcontroller.swift
@@ -0,0 +1,16 @@
+//
+//  UserListViewcontroller.swift
+//  ChatAppTraining
+//
+//  Created by USER on 2022/09/28.
+//
+
+import UIKit
+
+class UserListViewcontroller: UIViewController {
+    @IBOutlet weak var userListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
